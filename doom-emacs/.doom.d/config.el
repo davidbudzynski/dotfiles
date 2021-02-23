976174cb172425fb7e03355970e0523ec441f35f
David Budzynski <david.budzynsky@gmail.com>
2021-02-23 17:02:06 +0000

    increase the font size

--- doom-emacs/.doom.d/config.el
+++ doom-emacs/.doom.d/config.el
@@ -19,8 +19,8 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "SFMono Nerd Font Mono" :size 14 :weight 'medium)
-       doom-variable-pitch-font (font-spec :family "SFMono Nerd Font Mono" :size 15))
+(setq doom-font (font-spec :family "SFMono Nerd Font Mono" :size 15 :weight 'medium)
+       doom-variable-pitch-font (font-spec :family "SFMono Nerd Font Mono" :size 16))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
