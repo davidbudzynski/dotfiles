ceb256185f59cfe1135bd6689234d2e11a55c8ad
David Budzynski <david.budzynsky@gmail.com>
2021-03-13 21:02:55 +0000

    implement the new way of adding floating win rules

--- qtile/.config/qtile/config.py
+++ qtile/.config/qtile/config.py
@@ -26,7 +26,7 @@
 
 from typing import List  # noqa: F401
 from libqtile import bar, layout, widget, hook, qtile
-from libqtile.config import Click, Drag, Group, Key, Screen, ScratchPad, DropDown
+from libqtile.config import Click, Drag, Group, Key, Screen, ScratchPad, DropDown, Match
 from libqtile.lazy import lazy
 from libqtile.utils import guess_terminal
 from libqtile.widget import CurrentLayoutIcon
@@ -244,26 +244,15 @@ bring_front_click = False
 cursor_warp = False
 floating_layout = layout.Floating(float_rules=[
     # Run the utility of `xprop` to see the wm class and name of an X client.
-    {'wmclass': 'confirm'},
-    {'wmclass': 'dialog'},
-    {'wmclass': 'download'},
-    {'wmclass': 'error'},
-    {'wmclass': 'file_progress'},
-    {'wmclass': 'notification'},
-    {'wmclass': 'splash'},
-    {'wmclass': 'toolbar'},
-    {'wmclass': 'confirmreset'},  # gitk
-    {'wmclass': 'makebranch'},  # gitk
-    {'wmclass': 'maketag'},  # gitk
-    {'wname': 'branchdialog'},  # gitk
-    {'wname': 'pinentry'},  # GPG key password entry
-    {'wmclass': 'ssh-askpass'},  # ssh-askpass
-    # {'wmclass': 'mpv'},  # mpv
-    # firefox closing tab prompt (change?)
-    {'wname': 'Quit and close tabs?'},
-    {'wname': 'Close tabs?'},
-    {"wmclass": "flameshot"},
-    {"wname": "Picture-in-Picture"}
+    *layout.Floating.default_float_rules,
+    Match(wm_class = "confirmreset"),  # gitk
+    Match(wm_class = "makebranch"),  # gitk
+    Match(wm_class = "maketag"),  # gitk
+    Match(wm_class = "ssh-askpass"),  # ssh-askpass
+    Match(title = "branchdialog"),  # gitk
+    Match(title = "pinentry"),  # GPG key password entry
+    Match(wm_class = "flameshot"),
+    Match(title = "Picture-in-Picture"),
 ])
 auto_fullscreen = True
 focus_on_window_activation = "smart"
